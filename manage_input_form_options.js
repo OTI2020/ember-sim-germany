0e3e7e0364f00c7cf35aa4412d192f68e98b4b4a
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-02-19 11:27:37 +0100

    SHIFT+alt+F for syntax style

--- manage_input_form_options.js
+++ manage_input_form_options.js
@@ -1,13 +1,13 @@
- function showVegetationOptions() {
-        var vegetationType = document.getElementById("vegetationType").value;
-        var grasslandOptions = document.getElementById("grasslandOptions");
-        var forestOptions = document.getElementById("forestOptions");
+function showVegetationOptions() {
+    var vegetationType = document.getElementById("vegetationType").value;
+    var grasslandOptions = document.getElementById("grasslandOptions");
+    var forestOptions = document.getElementById("forestOptions");
 
-        if (vegetationType === "grassland") {
-            grasslandOptions.style.display = "block";
-            forestOptions.style.display = "none";
-        } else if (vegetationType === "forest") {
-            grasslandOptions.style.display = "none";
-            forestOptions.style.display = "block";
-        }
+    if (vegetationType === "grassland") {
+        grasslandOptions.style.display = "block";
+        forestOptions.style.display = "none";
+    } else if (vegetationType === "forest") {
+        grasslandOptions.style.display = "none";
+        forestOptions.style.display = "block";
     }
+}
