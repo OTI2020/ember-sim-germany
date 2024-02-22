a31ce7c301f29162de12302d098221dcfbd25e04
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-02-22 02:19:57 +0100

    new file for spread rate models

---
+++ simple_spread_rate_model.js
@@ -0,0 +1,19 @@
+// let spread_rate = 100;
+// Simple spread rate model used for testing. Will set the spread rate to a constant.
+function simple_spread_rate_model() {
+
+    /*
+    calculate_spread_rate(point, t0, t1) {
+      // r = Math.random()
+  
+      // point.theta = if t0 < 250 then 0 else Math.PI / 2.0
+      // point.forward_spread_rate = (0.3 + r*0.6) * 3
+      // point.flanking_spread_rate = (0.1 + r*0.2) * 3
+  
+      point.theta = 0;
+      return spread_rate = 1;
+    }*/
+    let spread_rate;
+    return spread_rate = 1;
+
+}
