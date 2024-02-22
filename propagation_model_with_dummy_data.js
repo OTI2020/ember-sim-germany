8f27f06e42acdd6f2319e54d2a5d41c1b7df216c
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-02-22 02:19:28 +0100

    integrate function for spread_rate

--- propagation_model_with_dummy_data.js
+++ propagation_model_with_dummy_data.js
@@ -120,7 +120,8 @@ function calculate_arrival_time(from_point, to_point, flat){ // t0, t1, flat) { 
         theta + Math.PI;
 
     // spread rate is in km/h, we need m/h
-    let r = 0.5 // (from_point.spread_rate * 1000.0) / 60.0; // only TEST data - MUST be changed!
+    let r = (spreadsimple_spread_rate_model() * 1000.0) / 60.0; // only TEST data - MUST be changed!
+    //let r = 0.5 // only TEST data - MUST be changed!
 
     // TODO HACK - this is to correct the ellipsis shape for high winds - need to find the problem with the algorithm! // this comment is from Australia
     r = r / ((-0.0097 * wind_speed) + 1.0558);
@@ -172,3 +173,11 @@ function calculate_arrival_time(from_point, to_point, flat){ // t0, t1, flat) { 
     console.log("tarrival: " + tarrival);
     return tarrival;
 }
+
+
+
+
+
+
+
+
