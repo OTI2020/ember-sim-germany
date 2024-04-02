0424b9c60cbd8b1e9af7112b8559e6b4c073fb54
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-04-02 18:01:36 +0200

    new file for timer

---
+++ javascript/timer.js
@@ -0,0 +1,22 @@
+console.log(document.getElementById("ignition_x").value);
+
+function test_run() {
+    var start = performance.now();
+    start_calculation()
+    var time = performance.now();
+    console.log('Dauer: ' + (time - start) + ' ms.');
+    console.clear();
+}
+
+
+function timer() {
+    var runtimes = [];
+
+    for (var i = 0; i < 100; i++) {
+        test_run();
+        runtimes.push(time - start);
+    }
+
+    console.log('list of runtimes:');
+    console.log(runtimes);
+}
